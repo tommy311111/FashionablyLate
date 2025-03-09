ee2c560ee8c8381d8a40fccda5619e916300bc40
y_has <yuko1640@gmail.com>
2025-03-09 14:57:59 +0900

    Update DatabaseSeeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -11,13 +11,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call(CategoriesTableSeeder::class);
-
-        // カテゴリーが存在しない場合は終了
+       
         if (Category::count() === 0) {
             return;
         }
 
-        // メモリ対策で5件ずつ (合計35件) 作成
         foreach (range(1, 7) as $i) {
             Contact::factory(5)->create();
         }
